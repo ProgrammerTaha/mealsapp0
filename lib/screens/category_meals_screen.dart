e2479f69fc90e839042f293355855814c4b0614f
programmertaha <programmer.taha.1y@gmail.com>
2021-02-21 05:59:43 +0300

    completing extracting information from dummy and adding meal item file

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../dummy_data.dart';
+
 class CategoryMealsScreen extends StatefulWidget {
   static final routName = '/CategoryMealsScreen';
 
@@ -11,14 +13,28 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
   @override
   Widget build(BuildContext context) {
     final routArg =
-        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+        ModalRoute.of(context)?.settings.arguments as Map;
     final categoryId = routArg['id'];
     final categoryTitle = routArg['title'];
+    final categoryMeal = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text('$categoryTitle'),
       ),
-      body: Container(),
+      body: ListView.builder(
+        itemCount: categoryMeal.length,
+        itemBuilder: (context, index) {
+          return Text('${categoryMeal[index].title}');
+        },
+      ),
     );
   }
 }
+
+
+
+
+
+
